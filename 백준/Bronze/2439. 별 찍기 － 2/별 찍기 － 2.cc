92daf6e090e43e4a19263d430b0848a35cce5fcb
Euna <74881955+JeongEuna@users.noreply.github.com>
2024-01-12 01:29:01 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int N;
+    std::cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N - i - 1; j++) {
+            std::cout << " ";
+        }
+        for (int k = 0; k < i + 1; k++) {
+            std::cout << "*";
+        }
+        
+        std::cout << '\n';
+    }
+}
